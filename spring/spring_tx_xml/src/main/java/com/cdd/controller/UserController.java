c01e72fea5d405880287a8b958c4927d5f2a62c2
IsMrChen <1374219106@qq.com>
2020-10-15 18:12:45 +0800

    xml_tx

---
+++ spring/spring_tx_xml/src/main/java/com/cdd/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.cdd.controller;
+
+import com.cdd.service.IUserService;
+import com.cdd.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author: Dandan.Chen
+ * @Description:
+ * @Date:Created in 14:16 2020/10/15
+ * @Modified By:
+ */
+public class UserController {
+
+}
---
+++ spring/spring_tx_xml/src/main/java/com/cdd/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cdd.service.impl;
+
+import com.cdd.dao.IUserDao;
+import com.cdd.dao.impl.UserDaoImpl;
+import com.cdd.domain.Account;
+import com.cdd.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author: Dandan.Chen
+ * @Description:
+ * @Date:Created in 13:00 2020/10/15
+ * @Modified By:
+ */
+public class UserServiceImpl implements IUserService {
+    private IUserDao userDao;
+    @Override
+    public List<Account> findAll() {
+        return userDao.findAll();
+    }
+    @Override
+    public void transfer(String outMan, String inMan, Integer money) {
+        userDao.out(outMan,money);
+        int i=1/0;
+        userDao.in(inMan,money);
+    }
+
+    public void setUserDao(IUserDao userDao) {
+        this.userDao = userDao;
+    }
+
+}
